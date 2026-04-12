df125a77b6c1213be7b01301f6a3244e94c02b36
naawnteam-sys <naawnteam@gmail.com>
2026-04-12 19:35:11 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'naawn-delivery-v106'; // تم التحديث هنا لإجبار مسح الكاش
+const CACHE_NAME = 'naawn-delivery-v107'; // تم التحديث هنا لإجبار مسح الكاش
 
 self.addEventListener('install', (e) => {
     self.skipWaiting();
